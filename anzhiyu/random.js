e9e80ac2ed2fe74e43994091be7f49eeb241c300
zecola <xyk816@gmail.com>
2024-03-31 17:34:19 +0800

    Site updated: 2024-03-31 17:34:18

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/03/31/hello-world/"];function toRandomPost(){
+var posts=["2024/03/31/hello-world/","2024/03/31/第一篇文章/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
